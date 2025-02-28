ce9c687a210c8c2f258f02f7a92f9af58713bedd
YovannyRn <yovannyn30@gmail.con>
2025-02-28 18:00:44 +0100

    registro

--- src/app/cliente/registro/registro.component.ts
+++ src/app/cliente/registro/registro.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { CredentialsService } from '../../services/auth/credentials.service';
 
 @Component({
   selector: 'app-registro',
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule],
   standalone: true,
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.scss'
 })
 export class RegistroComponent {
+
+  registroForm:FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private credentialService: CredentialsService
+  ){}
 
 }
