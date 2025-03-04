5ea1f4fa5424edb93d782b0eede4e21e4e6dd34d
YovannyRn <yovannyn30@gmail.con>
2025-03-04 13:15:30 +0100

    token

---
+++ src/app/services/auth/token.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  private readonly ACCESS_TOKEN_KEY: string = 'tienda_token'
+  private readonly REFRESH_TOKEN_KEY: string = 'tienda_refresh_token'
+
+  constructor(
+    private cookieService: CookieService,
+  ) { }
+
+  saveToken(token:string, refreshToken: string){
+    this.cookieService.set(this.ACCESS_TOKEN_KEY, token,{
+      path: "/",
+      secure: environment.tokenSecure, //En produccion tiene que estar en true 
+      sameSite: "Strict"
+    })
+
+    this.cookieService.set(this.REFRESH_TOKEN_KEY, refreshToken,{
+      path: "/",
+      secure: environment.tokenSecure, //En produccion tiene que estar en true 
+      sameSite: "Strict"
+    })
+  }
+
+  getAccessToken(){
+    return this.cookieService.get(this.ACCESS_TOKEN_KEY)
+  } 
+
+  getRefreshToken(){
+    return this.cookieService.get(this.REFRESH_TOKEN_KEY)
+  }
+
+  removeToken(){
+    this.cookieService.delete(this.ACCESS_TOKEN_KEY, '/', '', environment.tokenSecure, 'Strict') //si el secure es true, tiene que estar en true, si es false tiene que estar en false
+    this.cookieService.delete(this.REFRESH_TOKEN_KEY, '/', '', environment.tokenSecure, 'Strict')// es prduccion tiene que ser true 
+  }
+
+}
